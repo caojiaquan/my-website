693ec68e8eafbcf154404341672c9457797e0ece
caojiaquan <cjq95613>
2017-04-30 16:26:37 +0800

    My website Based on bootstrap  004

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/4/27 0027.
  */
 
-$(".navbar-right li").on('click',function(){
+$(".navbar-right li").on('click',function(){//导航菜单注册点击事件，调整滚动条高度
     $(".navbar-collapse.collapse.in").removeClass('in');
     var topWork =  $(".flexslider").innerHeight() +30;
     var topPhoto = topWork + $("#work").outerHeight(true);
@@ -30,17 +30,38 @@ $(".navbar-right li").on('click',function(){
         $(window).scrollTop(topFotter);
     }
 });
-$(window).scroll(function(){
-    var _this = this;
-    console.log($(this).scrollTop());
-    if($(this).scrollTop() >= 60){
-        $("#main-nav").addClass('nav-selected').css(
-            {
-                'top' : $(_this).scrollTop()
-            }
-        );
-    }else{
-        $("#main-nav").removeClass('nav-selected');
-    }
-
+$(window).scroll(function(){//监视滚动条的高度，让导航条的高度随着滚动条高度变化
+    var fn1 = function(){
+        if($(window).scrollTop() >= 60){
+            $("#main-nav").addClass('nav-selected').css(
+                {
+                    'top' : $(this).scrollTop()
+                }
+            );
+        }else{
+            $("#main-nav").removeClass('nav-selected');
+        }
+    };
+    debounce(fn1,400,1000);
 });
+function debounce(fn,delay,mustRunDelay){//防抖动函数
+    var timer = null;
+    var t_start;
+    return function(){
+        var context = this;
+        var args = arguments;
+        var t_curr = +new Date();
+        clearTimeout(timer);
+        if(!t_start){
+            t_start = t_curr;
+        }
+        if(t_curr - t_start >= mustRunDelay) {
+            fn.apply(context,args);
+            t_start = t_curr
+        } else {
+            timer = setTimeout(function(){
+                fn.apply(context,args);
+            },delay);
+        }
+    }();
+}
